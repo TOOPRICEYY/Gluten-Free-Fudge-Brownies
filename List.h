ba9cb700862c0b8b9716a2ddede397e081bd0a91
Gabriel Price <starfinderjetpacks@gmail.com>
2021-11-10 19:07:24 -0500

    sads

--- List.h
+++ List.h
@@ -70,10 +70,8 @@ public:
     p->datum = datum;
     p->next = first;
     p->prev = nullptr;
-    if(!empty()) {
-    first->prev = p;
+    if(!empty())first->prev = p;
     first = p;
-    }
     if(++siz==1){
       last = first;
     }
@@ -85,10 +83,8 @@ public:
     p->datum = datum;
     p->prev = last;
     p->next =  nullptr;
-    if(!empty()) {
-    last->next = p;
+    if(!empty()) last->next = p;
     last = p;
-    }
     if(++siz==1){
       first = last;
     }
