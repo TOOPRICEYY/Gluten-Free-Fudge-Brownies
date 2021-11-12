600d97c81409318f66c752429a9740db3ddfc19c
Gabriel Price <starfinderjetpacks@gmail.com>
2021-11-12 12:00:39 -0500

    isues

--- List.h
+++ List.h
@@ -193,6 +193,11 @@ public:
       if(node_ptr != rhs.node_ptr) {return true;}
       return false;
     }
+/*
+    Node * get(){
+      return node_ptr;
+    }
+  */
 
 
 
@@ -270,7 +275,7 @@ public:
   void print(){
     if(empty())return;
     for(Node * n = first; n; n=n->next){
-      cout << n->datum << ", ";
+      cout << n << ", ";
 
     }
     cout << endl;
--- List_tests.cpp
+++ List_tests.cpp
@@ -35,8 +35,8 @@ TEST(ittergarb) {
     int i = 3; while(i-->0)my_list.pop_back();
     List<int> list3(my_list);
     ASSERT_TRUE(list3.empty());
-    i = 0; while(i-->4)my_list.push_front(0);
-    i = 0; while(i-->4)my_list.pop_front();
+    i = 12; while(i-->0)my_list.push_front(0);
+    i = 12; while(i-->0)my_list.pop_front();
     List<int> list4(my_list);
     ASSERT_TRUE(list4.empty());
     list4.clear();
@@ -48,6 +48,54 @@ TEST(ittergarb) {
 
 }
 
+TEST(ittershit) {
+    // Add test code here
+    List<int> my_list;
+    List<int> list2(my_list);
+    ASSERT_TRUE(list2.empty());
+
+    my_list.push_back(2);
+    my_list.push_front(1);
+    my_list.push_back(3);
+    my_list.push_front(0);
+    
+    List<int> list5(my_list);
+    ASSERT_TRUE(!list5.empty());
+    
+    List<int>::Iterator iter = my_list.begin();
+    ++iter;
+    
+    ASSERT_TRUE(*iter==1);
+    my_list.pop_back();
+    
+    --iter;
+    ++iter;
+    ++iter;
+    ASSERT_TRUE(*iter==2);
+    --iter;
+    my_list.pop_front();
+    ASSERT_TRUE(*iter==1);
+
+    
+    int i = 2; while(i-->0)my_list.pop_back();
+    List<int> list3(my_list);
+    ASSERT_TRUE(list3.empty());
+    i = 4; while(i-->0)my_list.push_front(i);
+    i = 3; 
+    while(i-->0){
+        my_list.pop_front(); 
+        ASSERT_TRUE(my_list.size()==i+1); 
+        ASSERT_EQUAL(my_list.front(),3-i);
+    }
+    ASSERT_TRUE(list3.empty());
+    list3.clear();
+    list2.clear();
+    my_list.clear();
+    list5.clear();
+
+
+}
+/*
 TEST(test_ctor) {
     // Add test code here
     List<double> my_list;
@@ -55,22 +103,28 @@ TEST(test_ctor) {
     my_list.push_back(5.2);
     my_list.push_back(4.3);
     List<double>::Iterator iter = my_list.begin();
-    if(iter == iter){ };
     ++iter;
     my_list.push_front(6.8);
     my_list.push_front(1.2);
     my_list.pop_front();
     my_list.pop_back();
+    my_list.print();
+    
+    //cout << "1: "<<*iter <<endl;
+
     List<double> list2(my_list);
+    list2.print();
     List<double>::Iterator iter2 = list2.begin();
-    if(iter2 == iter2){ };
     ASSERT_TRUE(!list2.empty());
 
 
-    ASSERT_FALSE(iter == iter2);
+    //ASSERT_FALSE(iter == iter2);
+    cout << "1: "<<*iter << " 2 : "<<*iter2<<endl; 
+    cout << "1: "<<iter.get() << " 2 : "<<iter2.get()<<endl; 
+
     my_list.clear();
     list2.clear();    
 }
-
+*/
 TEST_MAIN()
 
